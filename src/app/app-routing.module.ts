64afeb90feedbed93116c215efd43d611583484b
JOSEEAND <josenun81@gmail.com>
2023-07-14 09:35:16 -0600

    laboratorio5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './seguridad/login/login.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { MenuComponent } from './shared/menu/menu.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import { SharedComponent } from './shared/shared.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'main', component: HeaderComponent },
-  { path: 'main1', component: MenuComponent },
-  { path: 'main2', component: FooterComponent },
+  { path: 'main', component: SharedComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
